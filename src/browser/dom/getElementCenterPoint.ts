398eb105a325d7644816b74b9a33b899976f74f6
hutyxxx <55497653+hutyxxx@users.noreply.github.com>
2021-02-22 18:18:24 +0800

    add current center

--- src/browser/dom/getElementCenterPoint.ts
+++ src/browser/dom/getElementCenterPoint.ts
@@ -3,4 +3,8 @@ export const getElementCenterPoint = (el: HTMLElement) => {
   const x = (left + right) / 2;
   const y = (top + bottom) / 2;
   return { x, y, top, bottom, left, right };
+  const cx = Math.abs((right - left) / 2);
+  const cy = Math.abs((top - bottom) / 2);
+  return { x, y, top, bottom, left, right, cx, cy };
 };
+
