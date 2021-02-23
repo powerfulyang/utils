928bfb88ec05ed04392d9e4e120fb05053ea4153
powerfulyang <i@powerfulyang.com>
2021-02-23 20:51:49 +0800

    fix: typo

--- src/browser/dom/getElementCenterPoint.ts
+++ src/browser/dom/getElementCenterPoint.ts
@@ -2,7 +2,6 @@ export const getElementCenterPoint = (el: HTMLElement) => {
   const { left, right, top, bottom } = el.getBoundingClientRect();
   const x = (left + right) / 2;
   const y = (top + bottom) / 2;
-  return { x, y, top, bottom, left, right };
   const cx = Math.abs((right - left) / 2);
   const cy = Math.abs((top - bottom) / 2);
   return { x, y, top, bottom, left, right, cx, cy };
