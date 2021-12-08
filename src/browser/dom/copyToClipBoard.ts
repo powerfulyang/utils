d7f2a46356a77851afdf7afb5a2f785d0a5580c7
powerfulyang <powerfulyang@qq.com>
2021-12-08 15:53:22 +0800

    fix: copyToClipBoard

--- src/browser/dom/copyToClipBoard.ts
+++ src/browser/dom/copyToClipBoard.ts
@@ -1,12 +1,12 @@
 export const copyToClipBoard = (state: string | Blob) => {
   if (state instanceof Blob) {
     const item = new ClipboardItem({
-      blob: Promise.resolve(state),
+      [state.type]: state,
     });
     return navigator.clipboard.write([item]);
   }
   const item = new ClipboardItem({
-    text: Promise.resolve(state),
+    'text/plain': new Blob([state], { type: 'text/plain' }),
   });
   return navigator.clipboard.write([item]);
 };
