fdfada0245e3884e76747aad7cfe03543e141c84
powerfulyang <powerfulyang@qq.com>
2023-03-21 17:06:17 +0800

    chore(test): html2md

--- src/esm-only/html2md.spec.ts
+++ src/esm-only/html2md.spec.ts
@@ -1,18 +1,8 @@
 import { html2md } from './html2md';
 
 describe('html2md', () => {
-  it('should convert html to markdown', async () => {
+  it('should convert table to markdown', async () => {
     const html = `
-    <ul>
-        <li><input type="checkbox">Chrome Extensions 可以进入 <code class="MarkdownContainer_inline-code__HOhIb cursor-text">chrome://extensions/shortcuts</code> 地址设置快捷键，比如给 <code class="MarkdownContainer_inline-code__HOhIb cursor-text">Google 翻译</code> 加一个快捷键，这样就不用每次点图标了。</li>
-    </ul>
-    <ul class="MarkdownContainer_contains-task-list__5pRp9">
-      <li>还粗略了解了 Chrome Extensions 的开发
-        <ul>
-            <li>schema: <a rel="noreferrer" class="link" target="_blank" href="https://json.schemastore.org/chrome-manifest">https://json.schemastore.org/chrome-manifest</a></li>
-        </ul>
-      </li>
-    </ul>
     <table class="table">
       <thead>
         <tr>
@@ -36,16 +26,63 @@ describe('html2md', () => {
     </table>
     `;
     const md = await html2md(html);
-    expect(md)
-      .toBe(`+ [ ] Chrome Extensions 可以进入 \`chrome://extensions/shortcuts\` 地址设置快捷键，比如给 \`Google 翻译\` 加一个快捷键，这样就不用每次点图标了。
-
-- 还粗略了解了 Chrome Extensions 的开发
-
-  + schema: [https://json.schemastore.org/chrome-manifest](https://json.schemastore.org/chrome-manifest)
-
-| name    | type   | description |
+    expect(md).toBe(`| name    | type   | description |
 | ------- | ------ | ----------- |
 | name    | string | name        |
 | version | string | version     |`);
+  });
+
+  it('should convert ul to markdown', async () => {
+    const html = `
+    <ul>
+      <li>name</li>
+      <li>version</li>
+    </ul>
+    `;
+    const md = await html2md(html);
+    expect(md).toBe(`+ name
++ version`);
+  });
+
+  it('should convert ol to markdown', async () => {
+    const html = `
+    <ol>
+      <li>name</li>
+      <li>version</li>
+    </ol>
+    `;
+    const md = await html2md(html);
+    expect(md).toBe(`1. name
+2. version`);
+  });
+
+  it('should convert pre to markdown', async () => {
+    const html = `
+    <pre><code>console.log('hello world')</code></pre>
+    `;
+    const md = await html2md(html);
+    expect(md).toBe(`\`\`\`
+console.log('hello world')
+\`\`\``);
+  });
+
+  it('should convert p to markdown', async () => {
+    const html = `
+    <p>hello world</p>
+    `;
+    const md = await html2md(html);
+    expect(md).toBe(`hello world`);
+  });
+
+  it('should convert checkbox to markdown', async () => {
+    const html = `
+    <ul>
+      <li><input type="checkbox" checked="checked">name</li>
+      <li><input type="checkbox">version</li>
+    </ul>
+    `;
+    const md = await html2md(html);
+    expect(md).toBe(`+ [x] name
++ [ ] version`);
   });
 });
