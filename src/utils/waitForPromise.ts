98cbb2125ea6a0b29b4c626210a25de15ced579c
powerfulyang <powerfulyang@qq.com>
2023-08-22 17:38:07 +0800

    feat: waitForPromise.ts

--- src/utils/waitForPromise.ts
+++ src/utils/waitForPromise.ts
@@ -1,6 +1,6 @@
 import { waitFor } from '@/utils/waitFor';
 
 export const waitForPromise = async <T>(promise: Promise<T>, ms = 1000) => {
-  const results = await Promise.allSettled([promise, waitFor(ms)]);
+  const results = await Promise.all([promise, waitFor(ms)]);
   return results[0];
 };
